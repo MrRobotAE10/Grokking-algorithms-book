6e25cc322ad616f2ca98b58a8b94a1b60f18cd09
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-07 23:21:08 -0500

    Quicksort file

---
+++ Chapter4/quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    pivot = arr[0]
+    less = [i for i in arr[1:] if i <= pivot]
+    greater = [i for i in arr[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([2,5,1,23,5,2,190,2]))
+

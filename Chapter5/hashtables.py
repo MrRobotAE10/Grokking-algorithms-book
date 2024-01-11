7c08af5acf45b6c843415dac4ef6dfa0ec305ec3
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-10 20:00:51 -0500

    Chapter 5, hashtable file

---
+++ Chapter5/hashtables.py
@@ -0,0 +1,7 @@
+# A hash table (hash function + an array together)
+book = dict() # Dictionaries is a hash table in python
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["avocado"] = 1.49
+
+print(book)

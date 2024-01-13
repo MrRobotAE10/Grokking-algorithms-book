917fb9642162220e84a6c0d50697f7de6e31f67c
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-13 16:34:32 -0500

    Simple graph

--- Chapter6/breadthFisrt.py
+++ Chapter6/breadthFisrt.py
@@ -3,4 +3,12 @@
 # 1. Is there a path from Node A to Node B
 # 2. What's the shortest path from node A to node B
 
-
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["tom", "jonny"]
+graph["anuj"] = []  
+graph["peggy"] = []
+graph["tom"] = []
+graph["jonny"] = []

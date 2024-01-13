6ad378ac87a89f0a8a09eb678b3d69eaa1791cc2
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-13 17:03:13 -0500

    Breadth first search algorithm implementation

--- Chapter6/breadthFisrt.py
+++ Chapter6/breadthFisrt.py
@@ -2,7 +2,10 @@
 # Answers two type of questions
 # 1. Is there a path from Node A to Node B
 # 2. What's the shortest path from node A to node B
+from collections import deque
 
+def is_person_seller(name):
+    return name[-1] == "m" # If person names end with m
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
 graph["bob"] = ["anuj", "peggy"]
@@ -12,3 +15,16 @@ graph["anuj"] = []
 graph["peggy"] = []
 graph["tom"] = []
 graph["jonny"] = []
+
+search_queue = deque() # Creates a queue
+search_queue += graph["you"] # Add all my neighbors to the queue
+
+while search_queue:
+    person = search_queue.popleft()
+    if is_person_seller(person):
+        print(f"{person} is a mango seller!")
+        break 
+    else:
+        search_queue += graph[person]
+else:
+    print("No mango seller was found")

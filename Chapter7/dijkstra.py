457df5da45f11548d6454b3253c4a65356fa3550
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-17 23:32:01 -0500

    Chapter 7, dijkstra algorithm

---
+++ Chapter7/dijkstra.py
@@ -0,0 +1,14 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}

76816eef3c0aa415b81432fe6749835e1920e8c7
MrRobotAE10 <sucoandchino@gmail.com>
2024-01-22 22:54:41 -0500

    Set up for the set covering problem

---
+++ Chapter8/set_covering_problem.py
@@ -0,0 +1,10 @@
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"]) # Sets dont allow duplicates
+
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+final_stations = set()

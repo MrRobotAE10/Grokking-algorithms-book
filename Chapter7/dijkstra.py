a2537620263121567f7b4e3aa64268d68a257734
Esteban Campoverde <sucoandchino@gmail.com>
2024-02-08 12:32:42 -0500

    Reimplementation of dijkstra's algorithm with some extra comments

--- Chapter7/dijkstra.py
+++ Chapter7/dijkstra.py
@@ -1,48 +1,40 @@
-# Finds the path with the smallest weight
+# Finds the path with the smallest weight, doesn't work on negative-weight (use Bellman-Ford algorithm instead)
 
 # Steps:
 # 1. Find the cheapest node
 # 2. Update the costs of the neighbors of this node
 # 3. Repeat until you've done this for every node
 # 4. Calculate the final path
-graph = {}
-graph["you"] = ["alice", "bob", "claire"]
 
+# One hash table, Four nested hash tables
+graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
-
 graph["a"] = {}
 graph["a"]["fin"] = 1
 graph["b"] = {}
 graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
-
 graph["fin"] = {}
 
+# Costs hash table
 infinity = float("inf")
 costs = {}
 costs["a"] = 6
 costs["b"] = 2
 costs["fin"] = infinity
 
+# Parents hash table
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
+# Array to keep track of the nodes already processed
 processed = []
 
-def find_lowest_cost_node(costs):
-    lowest_cost = float("inf")
-    lowest_cost_node = None
-    for node in costs:
-        cost = costs[node]
-        if cost < lowest_cost and node not in processed:
-            lowest_cost = cost
-            lowest_cost_node = node
-    return lowest_cost_node
-
+# Implementation
 node = find_lowest_cost_node(costs)
 while node is not None:
     cost = costs[node]

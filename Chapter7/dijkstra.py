3478ed3c1312bb0217ca7b65978de37e565d87f0
Esteban Campoverde <sucoandchino@gmail.com>
2024-02-08 13:10:01 -0500

    Implementation of the funcion find_lowest_cost_node

--- Chapter7/dijkstra.py
+++ Chapter7/dijkstra.py
@@ -34,6 +34,17 @@ parents["fin"] = None
 # Array to keep track of the nodes already processed
 processed = []
 
+# Helper function
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
 # Implementation
 node = find_lowest_cost_node(costs)
 while node is not None:
